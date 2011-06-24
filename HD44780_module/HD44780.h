7c3969ef80dd19ee17e630c9f5ebf6857050e8cb
Stuart Cording <codinghead@gmail.com>
2011-06-24 11:35:51 +0200

    Added clarification to the comments describing the #defines.
    Signed-off-by: Stuart Cording <codinghead@gmail.com>

--- HD44780_module/HD44780.h
+++ HD44780_module/HD44780.h
@@ -58,40 +58,202 @@
 
 /*******************************************************************************
 * Summary:
-*   These defines are used by the user to define the settings they wish to use
-* in conjunction with the available HD44780 instructions
+*   Entry Mode Set HD44780 Setting "CURSORMOVE" - cursor moves after each
+*   character is written
+* See also:
+*   hd44780EntryModeSet()
 *******************************************************************************/
-                                        /* Entry Mode Set settings            */
 #define EMS_CURSORMOVE      0xFE
+
+/*******************************************************************************
+* Summary:
+*   Entry Mode Set HD44780 Setting "DISPLAYSHIFT" - displayed text moves after
+*   each character is written and cursor stays in current position
+* See also:
+*   hd44780EntryModeSet()
+*******************************************************************************/
 #define EMS_DISPLAYSHIFT    0xFF
+
+/*******************************************************************************
+* Summary:
+*   Entry Mode Set HD44780 Setting "DECREMENT" - cursor moves to the left after
+*   each character is written
+* See also:
+*   hd44780EntryModeSet()
+*******************************************************************************/
 #define EMS_DECREMENT       0xFD
+
+/*******************************************************************************
+* Summary:
+*   Entry Mode Set HD44780 Setting "INCREMENT" - cursor moves to the right after
+*   each character is written
+* See also:
+*   hd44780EntryModeSet()
+*******************************************************************************/
 #define EMS_INCREMENT       0xFF
-                                        /* Display On/Off Control settings    */
+
+/*******************************************************************************
+* Summary:
+*   Display On/Off Control HD44780 Setting "BLINKINGOFF" - cursor blinking
+*   is switched off
+* See also:
+*   hd44780DisplayControl()
+*******************************************************************************/
 #define DOFC_BLINKINGOFF    0xFE
+
+/*******************************************************************************
+* Summary:
+*   Display On/Off Control HD44780 Setting "BLINKINGON" - cursor blinking
+*   is switched on
+* See also:
+*   hd44780DisplayControl()
+*******************************************************************************/
 #define DOFC_BLINKINGON     0xFF
+
+/*******************************************************************************
+* Summary:
+*   Display On/Off Control HD44780 Setting "CURSOROFF" - cursor is switched off
+*   making it invisible
+* See also:
+*   hd44780DisplayControl()
+*******************************************************************************/
 #define DOFC_CURSOROFF      0xFD
+
+/*******************************************************************************
+* Summary:
+*   Display On/Off Control HD44780 Setting "CURSORON" - cursor is switched on
+*   making it visible
+* See also:
+*   hd44780DisplayControl()
+*******************************************************************************/
 #define DOFC_CURSORON       0xFF
+
+/*******************************************************************************
+* Summary:
+*   Display On/Off Control HD44780 Setting "DISPLAYOFF" - entire display is
+*   turned off making its content invisible - text remains in SRAM
+* See also:
+*   hd44780DisplayControl()
+*******************************************************************************/
 #define DOFC_DISPLAYOFF     0xFB
+
+/*******************************************************************************
+* Summary:
+*   Display On/Off Control HD44780 Setting "DISPLAYON" - entire display is
+*   turned on making its content visible. Any text in SRAM will be redisplayed
+* See also:
+*   hd44780DisplayControl()
+*******************************************************************************/
 #define DOFC_DISPLAYON      0xFF
-                                        /* Cursor or Display Shift settings   */
+
+/*******************************************************************************
+* Summary:
+*   Cursor or Display Shift HD44780 Setting "SHIFTLEFT" - moves the cursor to
+*   the left, decrementing the Address Counter (AC). Does not change the content
+*   of the display
+* See also:
+*   hd44780ShiftControl()
+*******************************************************************************/
 #define CODS_SHIFTLEFT      0xFB
+
+/*******************************************************************************
+* Summary:
+*   Cursor or Display Shift HD44780 Setting "SHIFTRIGHT" - moves the cursor to
+*   the right, incrementing the Address Counter (AC). Does not change the
+*   content of the display
+* See also:
+*   hd44780ShiftControl()
+*******************************************************************************/
 #define CODS_SHIFTRIGHT     0xFF
+
+/*******************************************************************************
+* Summary:
+*   Cursor or Display Shift HD44780 Setting "CURSORMOVE" - only the cursor moves
+*   in the required direction
+* See also:
+*   hd44780ShiftControl()
+*******************************************************************************/
 #define CODS_CURSORMOVE     0xF7
+
+/*******************************************************************************
+* Summary:
+*   Cursor or Display Shift HD44780 Setting "DISPLAYSHIFT" - content of display
+*   move in the required direction
+* See also:
+*   hd44780ShiftControl()
+*******************************************************************************/
 #define CODS_DISPLAYSHIFT   0xFF
-                                        /* Function Set settings              */
+
+/*******************************************************************************
+* Summary:
+*   Function Set HD44780 Setting "5X8DOTS" - defines character RAM dimension to
+*   5x8 pixels when creating own characters
+* See also:
+*   hd44780FunctionSet()
+*******************************************************************************/
 #define FS_5X8DOTS          0xFD
+
+/*******************************************************************************
+* Summary:
+*   Function Set HD44780 Setting "5X10DOTS" - defines character RAM dimension to
+*   5x10 pixels when creating own characters
+* See also:
+*   hd44780FunctionSet()
+*******************************************************************************/
 #define FS_5X10DOTS         0xFF
+
+/*******************************************************************************
+* Summary:
+*   Function Set HD44780 Setting "1LINE" - defines that the display has only a
+*   single line
+* See also:
+*   hd44780FunctionSet()
+*******************************************************************************/
 #define FS_1LINE            0xF7
+
+/*******************************************************************************
+* Summary:
+*   Function Set HD44780 Setting "2LINE" - defines that the display has two
+*   lines, or that the LCD controller is reponsible for 2 lines of a 4-line
+*   LCD display
+* See also:
+*   hd44780FunctionSet()
+*******************************************************************************/
 #define FS_2LINE            0xFF
+
+/*******************************************************************************
+* Summary:
+*   Function Set HD44780 Setting "4BITBUS" - defines that the display is
+*   connected to the host CPU/MCU via a 4-bit data bus
+* See also:
+*   hd44780FunctionSet()
+*******************************************************************************/
 #define FS_4BITBUS          0xEF
+
+/*******************************************************************************
+* Summary:
+*   Function Set HD44780 Setting "8BITBUS" - defines that the display is
+*   connected to the host CPU/MCU via an 8-bit data bus
+* See also:
+*   hd44780FunctionSet()
+*******************************************************************************/
 #define FS_8BITBUS          0xFF
 
 /*******************************************************************************
 * Summary:
-*   These defines are used by the user to define the CGRAM font size when 
-* creating new fonts
+*   This defines that the CGRAM font has 8 lines of data when creating new fonts
+* See also:
+*   hd44780WriteCGRAM()
 *******************************************************************************/
 #define CGRAMFONT_5X8       8
+
+/*******************************************************************************
+* Summary:
+*   This defines that the CGRAM font has 10 lines of data when creating new
+*   fonts
+* See also:
+*   hd44780ReadCGRAM()
+*******************************************************************************/
 #define CGRAMFONT_5X10      10
 
 /*******************************************************************************
@@ -166,7 +328,7 @@ typedef HD44780OBJ * HHD44780;
 * New data type HD44780CLONE
 * Description:
 *   Holds a list of HD44780 clone chip sets so that we can perform the 
-* instruction initialisation depending on the chip set being used
+*   instruction initialisation depending on the chip set being used
 *******************************************************************************/
 typedef enum HD44780CLONETYPE
 {
@@ -221,7 +383,7 @@ unsigned char       hd44780ReturnHome(HHD44780 const    hHd44780);
 unsigned char       hd44780EntryModeSet(HHD44780 const     hHd44780,
                                      unsigned char      entryMode);
 unsigned char       hd44780DisplayControl(HHD44780 const    hHd44780,
-                                          unsigned char     displayOnOffControl);
+                                         unsigned char     displayOnOffControl);
 unsigned char       hd44780ShiftControl(HHD44780 const  hHd44780,
                                         unsigned char   shiftControl);
 unsigned char       hd44780FunctionSet(HHD44780 const   hHd44780,
