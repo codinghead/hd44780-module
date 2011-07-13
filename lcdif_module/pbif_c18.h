a0419b05700e951f716efda122ec5ecf77eb54b2
Stuart Cording <codinghead@gmail.com>
2011-07-13 14:34:19 +0200

    Enhanced comments on the structure PBIFOBJ. Added abstraction for the enable pin and comments for new PBIFLCDENOBJ structure.
    Signed-off-by: Stuart Cording <codinghead@gmail.com>

--- lcdif_module/pbif_c18.h
+++ lcdif_module/pbif_c18.h
@@ -19,12 +19,10 @@
 * All contents within this file are 'public' and to be used by end user
 *
 * Filename : pbif_c18.h
-* Version : V0.01
 * Programmer(s) : Stuart Cording aka. CODINGHEAD
 * 
 ********************************************************************************
 * Note(s) : 
-* V0.01 -   First cut of this module
 *
 *******************************************************************************/
 
@@ -40,7 +38,9 @@
 /*******************************************************************************
 *                                 INCLUDE FILES
 *******************************************************************************/
+#ifdef __18CXX
 #include <p18cxxx.h>
+#endif
 
 
 /*******************************************************************************
@@ -65,20 +65,43 @@
 /*******************************************************************************
 * New data type PBIFOBJ                                                    
 * Description:
-*   This decribes the object for the parallel bus interface
+*   This decribes the object for the parallel bus interface. It requires the
+* following elements:
+* - The LAT register to which the R/W pin is connected
+* - The bit number to which the R/W pin is connected (counting up from 0)
+* - The LAT register to which the RS pin is connected
+* - The bit number to which the RS pin is connected (counting up from 0)
+* - The LAT register to which the data pins are connected
+* - The PORT register to which the data pins are connected
+* - The TRIS register to which the data pins are connected
+* - A mask of 4 or 8 bits to define which of the data pins from the GPIO port
+*   are connected to the LCD interface (must be consecutive)
+* - A mutex variable used by the LCDIF module only
 *******************************************************************************/
 typedef struct PBIFOBJTYPE {
     volatile near unsigned char   * RW_LAT;
-    unsigned char				    RW_BIT;
+    unsigned char                   RW_BIT;
     volatile near unsigned char   * RS_LAT;
-    unsigned char				    RS_BIT;
+    unsigned char                   RS_BIT;
     volatile near unsigned char   * DATA_LAT;
     volatile near unsigned char   * DATA_PORT;
     volatile near unsigned char   * DATA_TRIS;
-    unsigned char				    DATA_MASK;
-    char 			                mutex;
+    unsigned char                   DATA_MASK;
+    char                            mutex;
 } PBIFOBJ;
 
+/*******************************************************************************
+* New data type PBIFLCDENOBJ
+* Description:
+*   This decribes the object for the enable pin for an individual LCD interdace
+* It requires the following elements:
+* - The LAT register to which the E pin is connected
+* - The bit number to which the E pin is connected (counting up from 0)
+*******************************************************************************/
+typedef struct PBIFLCDENOBJTYPE {
+    volatile near unsigned char   * E_LAT;
+    unsigned char                   E_BIT;
+} PBIFLCDENOBJ;
 
 /*******************************************************************************
 *                                GLOBAL VARIABLES
